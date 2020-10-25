c3cd91f6619f0a0ce0de4affeaba407a459fd6bc
bigrushdog <randall.rushing@gmail.com>
2020-10-24 17:48:33 -0700

    Pulse initial checkin for Android 13 [1/2]
    @neobuddy89:
    * Adapted for A13 - removed dagger dependencies, PulseController interfaces, unused listeners.
    * Pulses disabled by default - following AOSP / LOS behavior.
    * This includes few of NPE fixes and improvements.
    * Android 12 removed colored notifications in ff165814e10cf757981d1e0254bd88473dff126e,
      so retire pulse color based on album art till we find other way.
    
    ----
    ezio:
    
    big squash here to handle Android 11 changes,
    check external pulse tree to know who contributed
    to this epic feature that Randall made back in the days
    (yeah, we're getting old) :D
    
    some mentions: neobuddy89, Alex Cruz, LorDClockaN, varund7726
    
    ps: now when the the new qs media player notification is enabled,
    a different call to media processor is done by the mediadatamanager
    in packages/SystemUI/src/com/android/systemui/media/, that's why the
    need of MediaDataManager.Listener in NotificationMediaManager.
    reminder for myself:  we can acces properties of kotlin "data" classes
    like MediaData.kt just calling a built-in getter (like getBackgroundColor
    for the backgroundColor prop). For reference, it's also possible to call
    straihgt mediaData.background color but only after adding the @JvmField
    flag to the prop declaration
    
    Change-Id: I0205f8ba6b8883012d1b809de9a33f6391c4cf0c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -167,6 +167,8 @@ import com.android.systemui.plugins.PluginManager;
 import com.android.systemui.plugins.qs.QS;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.power.domain.interactor.PowerInteractor;
+import com.android.systemui.pulse.PulseControllerImpl;
+import com.android.systemui.pulse.VisualizerView;
 import com.android.systemui.qs.QSFragmentLegacy;
 import com.android.systemui.qs.QSPanelController;
 import com.android.systemui.qs.composefragment.QSFragmentCompose;
@@ -493,6 +495,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     private final ActivityStarter mActivityStarter;
 
     private GameSpaceManager mGameSpaceManager;
+    private final PulseControllerImpl mPulseController;
+    private VisualizerView mVisualizerView;
 
     private final DisplayMetrics mDisplayMetrics;
 
@@ -877,6 +881,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         }
         mEmergencyGestureIntentFactory = emergencyGestureIntentFactory;
         mWalletController = walletController;
+        mPulseController = new PulseControllerImpl(mContext, this,
+                mCommandQueue, mUiBgExecutor, mConfigurationController);
 
         mLockscreenShadeTransitionController = lockscreenShadeTransitionController;
         mStartingSurfaceOptional = startingSurfaceOptional;
@@ -1279,6 +1285,9 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         mCommandQueueCallbacks = mCommandQueueCallbacksLazy.get();
         mCommandQueue.addCallback(mCommandQueueCallbacks);
 
+        // this will initialize Pulse and begin listening for media events
+        mMediaManager.addCallback(mPulseController);
+
         // TODO: Deal with the ugliness that comes from having some of the status bar broken out
         // into fragments, but the rest here, it leaves some awkward lifecycle and whatnot.
         ShadeExpansionChangeEvent currentState =
@@ -1420,6 +1429,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                 }
             });
         }
+
+        mVisualizerView = (VisualizerView) getNotificationShadeWindowView().findViewById(R.id.visualizerview);
 
         mReportRejectedTouch = getNotificationShadeWindowView()
                 .findViewById(R.id.report_rejected_touch);
@@ -2487,6 +2498,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         // bar.
         mKeyguardStateController.notifyKeyguardGoingAway(true);
         mCommandQueue.appTransitionPending(mDisplayId, true /* forced */);
+        mPulseController.notifyKeyguardGoingAway();
         updateScrimController();
     }
 
@@ -2573,6 +2585,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         }
 
         mShadeSurface.setDozing(mDozing, animate);
+        mPulseController.setDozing(mDozing);
         Trace.endSection();
     }
 
@@ -3415,6 +3428,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                     updateDozingState();
                     checkBarModes();
                     updateScrimController();
+                    mPulseController.setKeyguardShowing(mState == StatusBarState.KEYGUARD);
                     Trace.endSection();
                 }
 
@@ -3461,6 +3475,10 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                     Trace.endSection();
                 }
             };
+
+    public VisualizerView getLsVisualizer() {
+        return mVisualizerView;
+    }
 
     private final BatteryController.BatteryStateChangeCallback mBatteryStateChangeCallback =
             new BatteryController.BatteryStateChangeCallback() {
