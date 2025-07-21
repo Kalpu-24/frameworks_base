ebac0a2636c70cbc360d91290b7998b7ee4f03f9
Ghosuto <clash.raja10@gmail.com>
2025-07-21 16:43:14 +0000

    test
    Change-Id: If585b6f314626b7364053b543e3a59e593bbd78c

--- packages/SystemUI/src/com/android/systemui/media/MediaSessionManager.kt
+++ packages/SystemUI/src/com/android/systemui/media/MediaSessionManager.kt
@@ -53,10 +53,6 @@ class MediaSessionManager private constructor() {
     fun onAlbumArtChanged(drawable: Drawable) {
         listenerManager.notify { it.onAlbumArtChanged(drawable) }
     }
-    
-    fun onMediaColorsChanged(color: Int) {
-        listenerManager.notify { it.onMediaColorsChanged(color) }
-    }
 
     fun onMediaColorsChanged(color: Int) {
         listenerManager.notify { it.onMediaColorsChanged(color) }
