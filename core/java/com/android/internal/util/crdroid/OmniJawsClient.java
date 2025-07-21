55a5b6f322db218c76bc22a7a356aa9de244dbb0
Ghosuto <clash.raja10@gmail.com>
2025-07-21 16:41:58 +0000

    n
    Change-Id: Icde19f68a5ea21d16604f37529d7b225035021c0

--- core/java/com/android/internal/util/crdroid/OmniJawsClient.java
+++ core/java/com/android/internal/util/crdroid/OmniJawsClient.java
@@ -17,26 +17,6 @@
 */
 package com.android.internal.util.crdroid;
 
-/*
-* Copyright (C) 2021 The OmniROM Project
-* Copyright (C) 2025 AxionOS Project
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*
-*/
-package com.android.internal.util.android;
-
 import android.content.*;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
