204dd80b9bb9c7dd3f6964345d61bbba788cfce0
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-21 13:10:49 +0800

    [BUGFIX] fix lockscreen elements randomly disappearing
    07-21 10:53:00.892  2046  2046 D KeyguardTransitionRepository: (Internal) Setting current transition info: TransitionInfo(ownerName=FromGoneTransitionInteractor(keyguard interactor says keyguard is showing), from=GONE, to=LOCKSCREEN, animated)
    07-21 10:53:00.924  2046  2046 I KeyguardTransitionRepository: Transition still active: TransitionStep(from=GONE, to=DOZING, value=0.6334405, transitionState=RUNNING, ownerName=FromGoneTransitionInteractor(Sleep transition triggered)), canceling
    07-21 10:53:00.924  2046  2046 I KeyguardTransitionRepository: CANCELED transition: TransitionStep(from=GONE, to=DOZING, value=0.6334405, transitionState=CANCELED, ownerName=FromGoneTransitionInteractor(Sleep transition triggered))
    07-21 10:53:00.924  2046  2046 I KeyguardTransitionRepository: STARTED transition: TransitionStep(from=GONE, to=LOCKSCREEN, value=0.6334405, transitionState=STARTED, ownerName=FromGoneTransitionInteractor(keyguard interactor says keyguard is showing))
    07-21 10:53:00.925  2046  2046 I KeyguardTransitionRepository: Transition still active: TransitionStep(from=GONE, to=LOCKSCREEN, value=0.6334405, transitionState=RUNNING, ownerName=FromGoneTransitionInteractor(keyguard interactor says keyguard is showing)), canceling
    07-21 10:53:00.925  2046  2046 I KeyguardTransitionRepository: CANCELED transition: TransitionStep(from=GONE, to=LOCKSCREEN, value=0.6334405, transitionState=CANCELED, ownerName=FromGoneTransitionInteractor(keyguard interactor says keyguard is showing))
    07-21 10:53:00.925  2046  2046 I KeyguardTransitionRepository: STARTED transition: TransitionStep(from=DOZING, to=GONE, value=0.6334405, transitionState=STARTED, ownerName=FromDozingTransitionInteractor(canDismissLockscreen()))
    07-21 10:53:01.148  2046  2046 I KeyguardTransitionRepository: FINISHED transition: TransitionStep(from=DOZING, to=GONE, value=1.0, transitionState=FINISHED, ownerName=FromDozingTransitionInteractor(canDismissLockscreen()))
    
    the lockscreen transition is being canceled by dozing to gone transition step, leaving the animation in a bad state where the lockscreen elements like clock, lock icon, shortcuts and indication text were stucked at low alpha or was reset to 0f alpha. fix the bug by preventing lockscreen from being canceled unless its bouncer.
    
    Change-Id: Ib791e00f427d29002da2339e6334dfd35fc90e26
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/data/repository/KeyguardTransitionRepository.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/data/repository/KeyguardTransitionRepository.kt
@@ -217,9 +217,22 @@ constructor(
                 Log.i(TAG, "Duplicate call to start the transition, rejecting: $info")
                 return@withContext null
             }
-            val isAnimatorRunning = lastAnimator?.isRunning() ?: false
+            val isAnimatorRunning = lastAnimator?.isRunning == true
             val isManualTransitionRunning =
                 updateTransitionId != null && lastStep.transitionState != TransitionState.FINISHED
+            val isLastToLockscreen = lastStep.to == KeyguardState.LOCKSCREEN
+            val isCancelingLockscreen =
+                isLastToLockscreen && info.to != KeyguardState.PRIMARY_BOUNCER
+
+            if ((isAnimatorRunning || isManualTransitionRunning) && isCancelingLockscreen) {
+                Log.i(
+                    TAG,
+                    "Preventing cancelation of active LOCKSCREEN transition unless going to PRIMARY_BOUNCER: " +
+                    "Active=$lastStep, Incoming=$info"
+                )
+                return@withContext null
+            }
+
             val startingValue =
                 if (isAnimatorRunning || isManualTransitionRunning) {
                     Log.i(TAG, "Transition still active: $lastStep, canceling")
