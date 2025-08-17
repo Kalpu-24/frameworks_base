1c05c9e67af156d0be0825c79b2e2934b5217d77
Abhay Singh Gill <abhaygill017@gmail.com>
2025-08-18 05:01:02 +0530

    SystemUI: Switch to compat text for data usage text
    To fix fonts not updating on that.
    Also use font size as 14.sp just like on legacy qs.
    
    CompatText was introduced in:-
    https://github.com/Neoteric-OS/android_frameworks_base/commit/347bbc817ecb29c16e4b7d2e2d5005254f73df48
    
    This is needed as compose's FontFamilyResolver provides us the stale font family whereas creating a new TypeFace resolves our issue and uses the current font family used by android.
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- packages/SystemUI/src/com/android/systemui/development/ui/compose/BuildNumber.kt
+++ packages/SystemUI/src/com/android/systemui/development/ui/compose/BuildNumber.kt
@@ -24,7 +24,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Text
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -41,6 +40,7 @@ import com.android.systemui.development.ui.viewmodel.BuildNumberViewModel
 import com.android.systemui.lifecycle.rememberViewModel
 import com.android.systemui.qs.footer.ui.viewmodel.FooterActionsDataUsageViewModel
 import com.android.systemui.res.R
+import com.android.systemui.text.CompatText
 
 @Composable
 fun BuildNumber(
@@ -60,7 +60,7 @@ fun BuildNumber(
         if (isVisible && dataUsageText != null) {
             val haptics = LocalHapticFeedback.current
             
-            Text(
+            CompatText(
                 text = dataUsageText,
                 modifier =
                     modifier
