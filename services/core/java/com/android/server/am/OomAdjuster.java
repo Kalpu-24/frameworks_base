730937d6a30574daeb9bb5d98291d8a23185505e
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-28 11:01:57 +0800

    [BOOST FRAMEWORK] structure finalization

--- services/core/java/com/android/server/am/OomAdjuster.java
+++ services/core/java/com/android/server/am/OomAdjuster.java
@@ -3660,11 +3660,7 @@ public class OomAdjuster {
             int processGroup;
             switch (curSchedGroup) {
                 case SCHED_GROUP_BACKGROUND:
-                    if (BoostAdjuster.isInPerfList(app.processName)) {
-                        processGroup = THREAD_GROUP_RESTRICTED;
-                    } else {
-                        processGroup = THREAD_GROUP_BACKGROUND;
-                    }
+                    processGroup = THREAD_GROUP_BACKGROUND;
                     break;
                 case SCHED_GROUP_TOP_APP:
                 case SCHED_GROUP_TOP_APP_BOUND:
