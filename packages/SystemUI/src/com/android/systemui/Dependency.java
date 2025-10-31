c445c3447101bfb83b92fd1015c63ba1c9b72d70
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-10-31 15:57:53 +0800

    SystemUI: Fixing scrimutils tricky state bugs
    Change-Id: I9507bdcd3697bd5e005a9dbf55522b5bbe1dac1e
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -56,6 +56,7 @@ import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.window.StatusBarWindowControllerStore;
 import com.android.systemui.tuner.TunerService;
+import com.android.systemui.util.ScrimUtils;
 
 import dagger.Lazy;
 
@@ -153,6 +154,7 @@ public class Dependency {
     @Inject Lazy<UserTracker> mUserTrackerLazy;
     @Inject Lazy<StatusBarWindowControllerStore> mStatusBarWindowControllerStoreLazy;
     @Inject Lazy<DozeScreenStateEx> mDozeScreenStateEx;
+    @Inject Lazy<ScrimUtils> mScrimUtils;
 
     @Inject
     public Dependency() {
@@ -201,6 +203,8 @@ public class Dependency {
                 StatusBarWindowControllerStore.class, mStatusBarWindowControllerStoreLazy::get);
         mProviders.put(
                 DozeScreenStateEx.class, mDozeScreenStateEx::get);
+        mProviders.put(
+                ScrimUtils.class, mScrimUtils::get);
         Dependency.setInstance(this);
     }
 
