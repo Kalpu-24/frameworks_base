429c5cb66bb9cc782ab7822a2df3a9c01fc15b04
Ghosuto <clash.raja10@gmail.com>
2025-11-13 16:13:01 +0000

    services: Update pref blacklist
    Change-Id: I8ee32c196011168a0d6b1f0fb60f7c94575d3fbc
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/am/AxUtils.java
+++ services/core/java/com/android/server/am/AxUtils.java
@@ -65,7 +65,7 @@ public class AxUtils {
         sPerfBlackList.add("org.lineage.settings");
         sPerfBlackList.add("co.aospa.sense");
         sPerfBlackList.add("io.chaldeaprjkt.gamespace");
-        sPerfBlackList.add("com.android.edge.bar");
+        sPerfBlackList.add("com.libremobileos.sidebar");
     }
 
     AxUtils() {
